707f19fa649751dcf0f741f9dbb4ae565792d5aa
Larina <49904492+Larinax999@users.noreply.github.com>
2025-02-22 00:40:44 +0700

    up to date

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-// add this to top of polyfills.845d83d94e685fd554f9.js
-let setTimeout=(func,time)=>{
-    if (!func.toString().includes("{if(9999===")) return window.setTimeout(func,time);
-    func();
-    return 0;
+// ==UserScript==
+// @name         all ticket loading bypass
+// @namespace    https://github.com/Larinax999/allticket-delay-bypass
+// @version      2025-02-21
+// @description  abc
+// @author       Larina
+// @match        https://www.allticket.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=allticket.com
+// @run-at       document-start
+// @grant        unsafeWindow
+// ==/UserScript==
+
+const setTimeoutX=window.setTimeout;
+unsafeWindow.setTimeout=function(...args){
+    const funcstr = args[0].toString();
+    if (funcstr.includes("{if(9999===")||funcstr.includes("validateBuyTicketBtn")) {
+        console.log(args[0],funcstr,args[1]);
+        args[1]=0;
+    }
+    return setTimeoutX.apply(this,args);
 };
